23f0568744a23c1ab7269db5045576a04556635c
R4mBLe_ <misharad2008@gmail.com>
2024-06-06 17:19:12 +0300

    port to 1.20.6

--- src/main/java/com/r4mble/scarygrannymod/common/item/ModItems.java
+++ src/main/java/com/r4mble/scarygrannymod/common/item/ModItems.java
@@ -9,6 +9,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import static net.minecraft.world.item.SwordItem.createAttributes;
+
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ScaryGrannyMod.MOD_ID);
     public static final RegistryObject<Item> ASHES = ITEMS.register("ashes",
@@ -18,6 +20,6 @@ public class ModItems {
     public static final RegistryObject<Item> ASHES_DIAMOND = ITEMS.register("ashes_diamond",
         () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> GRANNY_BAT = ITEMS.register("granny_bat",
-            () -> new SwordItem(Tiers.IRON, 10, 1.6f, new Item.Properties().defaultDurability(100)));
+            () -> new SwordItem(Tiers.IRON, new Item.Properties().attributes(createAttributes(Tiers.IRON, 7, -2.4f)).durability(100)));
 
 }
